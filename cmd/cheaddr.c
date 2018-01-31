b4c6a7ef9a1d73a3008e4a96b5f24c3410593f73
Alfred M. Szmidt <ams@gnu.org>
2018-01-31 05:07:19 +0100

    cmd/cheaddr: Cleanup.

--- cmd/cheaddr.c
+++ cmd/cheaddr.c
@@ -1,42 +1,46 @@
-#include <string.h>
-#include <stdlib.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>
+// cheaddr -- set Chaos address on an interface
+
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/ioctl.h>
+
 #include <chaos.h>
 
-/*
- * Enable an ethernet interface
- */
 int
 main(int argc, char **argv)
 {
 	int i;
-	extern int errno;
-#if defined(BSD42) || defined(linux)
-	struct chether che;
 
 	if (argc < 2) {
-	  printf("usage: %s <if-name> <node-number>\n", argv[0]);
-	  exit(1);
+		printf("usage: %s <if-name> <node-number>\n", argv[0]);
+		exit(1);
 	}
+
+#if NCHETHER
+	struct chether che;
+
 	strncpy(che.ce_name, argv[1], CHIFNAMSIZ);
 	sscanf(argv[2], "%o", &i);
 	che.ce_addr = i;
+
 	printf("Setting device %s to chaos address 0%o\n", che.ce_name, che.ce_addr);
-	if ((i = open(CHURFCDEV, 0)) < 0 ||
-	    ioctl(i, CHIOCETHER, &che) < 0)
-#else
+	if ((i = open(CHURFCDEV, 0)) < 0 || ioctl(i, CHIOCETHER, &che) < 0)
+		printf("Can't set chaos address on ethernet: %d\n", errno);
+#elif NCHIL
 	struct chiladdr ca;
 
 	ca.cil_device = atoi(argv[1]);
 	sscanf(argv[2], "%o", &i);
 	ca.cil_address = i;
-	printf("Setting device %d to 0%o\n", ca.cil_device, ca.cil_address);
-	if ((i = open(CHURFCDEV, 0)) < 0 ||
-	    ioctl(i, CHIOCILADDR, &ca) < 0)
-#endif
+
+	printf("Setting device %d to chaos address 0%o\n", ca.cil_device, ca.cil_address);
+	if ((i = open(CHURFCDEV, 0)) < 0 || ioctl(i, CHIOCILADDR, &ca) < 0)
 		printf("Can't set chaos address on ethernet: %d\n", errno);
+#endif
+
 	exit(0);
 }
