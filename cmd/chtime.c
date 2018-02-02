00f57ef00600eb0fc2f0682589f617a239b8feeb
Alfred M. Szmidt <ams@gnu.org>
2018-02-02 03:14:45 +0100

    cmd/chtime: Cleanup.

--- cmd/chtime.c
+++ cmd/chtime.c
@@ -1,40 +1,42 @@
+// chtime --- ---!!!
+
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <hosttab.h>
 #include <time.h>
+#include <unistd.h>
+
+#include <hosttab.h>
 #include <chaos.h>
 
 int
-main (int argc, char **argv)
+main(int argc, char **argv)
 {
-	int	fd, ret;
-	int	addr;
+	int fd, ret;
+	int addr;
 	unsigned short high, low;
-	long	now;
+	long now;
 
 	if (argc == 1) {
-		fprintf (stderr, "Usage: chtime host [ host ] ...\n");
-		exit (1);
+		fprintf(stderr, "usage: chtime host...\n");
+		exit(1);
 	}
 
 	while (--argc) {
 		argv++;
 		addr = chaos_addr(*argv, 0);
 		if (addr == 0) {
-			fprintf (stderr, "host %s unknown\n", *argv);
+			fprintf(stderr, "host %s unknown\n", *argv);
 			exit(1);
 		}
 		if ((fd = chopen(addr, "TIME", 0, 0, 0, 0, 0)) < 0) {
-			fprintf (stderr, "Host %s (0%o) is not responding\n", *argv, addr);
+			fprintf(stderr, "Host %s (0%o) is not responding\n", *argv, addr);
 			continue;
 		}
 
 		/* 32 bits of time */
-
-		ret = read (fd, &low, 2);
-		ret = read (fd, &high, 2);
+		ret = read(fd, &low, 2);
+		ret = read(fd, &high, 2);
 		now = ((long)high << 16) + (long)low;
 		now -= 60L*60*24*((1970-1900)*365L + 1970/4 - 1900/4);
 		printf("%20s (0%o):\t%s", *argv, addr, ctime(&now));
