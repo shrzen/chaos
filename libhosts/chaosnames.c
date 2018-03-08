7db9b679a2eca44b08792e59fec888fdababed8d
Alfred M. Szmidt <ams@gnu.org>
2018-03-08 03:33:10 +0100

    libhosts/chaosnames.c: Clean up, add proper option handling.

--- libhosts/chaosnames.c
+++ libhosts/chaosnames.c
@@ -1,11 +1,64 @@
+// chaosnames --- ---!!!
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
 
-int chaosnames(FILE *fp);
+extern int chaosnames(FILE *fp);
+
+void
+usage(void)
+{
+	fprintf(stderr, "usage: chaosnames [FILE]\n");
+	fprintf(stderr, "---!!!\n");
+	fprintf(stderr, "\n");
+	fprintf(stderr, "  -h             help message\n");
+}
 
 int
-main(void)
+main(int argc, char **argv)
 {
-	chaosnames(stdout);
-	exit(0);
+	int c = 0;
+	int errflg = 0;
+	FILE *fd = NULL;
+	int ret = 0;
+
+	while ((c = getopt(argc, argv, "h")) != -1) {
+		switch (c) {
+		case 'h':
+			usage();
+			exit(0);
+		case '?': default:
+			fprintf(stderr, "invalid option -- '-%c'\n", optopt);
+			errflg++;
+			break;
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (errflg) {
+		usage();
+		exit(1);
+	}
+
+	if (argc >= 2) {
+		usage();
+		exit (1);
+	}
+
+	if (argc == 0)
+		fd = stdout;
+	else {
+		fd = fopen(argv[0], "r");
+		if (fd == NULL) {
+			fprintf(stderr, "%s: %s\n", argv[0], strerror(errno));
+			exit (1);
+		}
+	}
+	ret = chaosnames(fd);
+
+	exit(ret);
 }
