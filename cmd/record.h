63ab353806a42100cc7d7c6aafa593b7cd1b5e80
Alfred M. Szmidt <ams@gnu.org>
2018-03-09 19:20:45 +0100

    record.h: Minor fixups.

--- cmd/record.h
+++ cmd/record.h
@@ -1,4 +1,3 @@
-#define RECMAGIC "RECORD STREAM VERSION 1\215"
 
 struct	record_stream {
 	FILE	*r_rfp;
@@ -6,17 +5,5 @@ struct	record_stream {
 	int	r_rlen;
 	int	r_wlen;
 };
-
-extern struct record_stream *recopen(int f, int mode);
-extern int recforce(struct record_stream *rp);
-extern int recclose(struct record_stream *rp);
-extern int recop(struct record_stream *rp);
-extern int reclength(struct record_stream *rp);
-extern int recread(struct record_stream *rp, char *buf, int len);
-extern int recchar(struct record_stream *rp);
-extern int recstart(struct record_stream *rp, int op, int len);
-extern int recwchar(struct record_stream *rp, int c);
-extern int recwrite(struct record_stream *rp, int op, char *buf, int len);
-extern int recrfileno(struct record_stream *rs);
-extern int recwfileno(struct record_stream *rs);
-extern int recflush(struct record_stream *rs);
+#define RECMAGIC "RECORD STREAM VERSION 1\215"
+extern struct record_stream *recopen();
