2d9e7e768442c3cdc1b2af91441871bb20b03c8b
Alfred M. Szmidt <ams@gnu.org>
2018-03-09 20:06:46 +0100

    chlinux/chlinux.h: Fixup for recent kernels.

--- chlinux/chlinux.h
+++ chlinux/chlinux.h
@@ -1,29 +1,30 @@
-#include <linux/errno.h>
-#include <linux/proc_fs.h>
-#include <linux/sched.h>
-#include <linux/signal.h>
-#include <linux/time.h>
 #include <linux/types.h>
+#include <linux/signal.h>
+#include <linux/errno.h>
+#include <linux/sched.h>
+#include <linux/proc_fs.h>
 
-#include "chncp/chuser.h"
-#include "chncp/chclock.h"
-#include "chncp/chsend.h"
-#include "chncp/chrcv.h"
-#include "chncp/chutil.h"
-#include "chunix/chtime.h"
-#include "chunix/challoc.h"
-#include "chunix/chether.h"
+#define verify_area(type, addr, size) access_ok(type, addr, size) ? 0 : -EFAULT
 
-int ch_size(char *p);
-void ch_free(char *p);
+///---???
+#define memcpy_fromfs copy_from_user
+#define memcpy_tofs copy_to_user
 
-#define cli() local_irq_disable()
-#define sti() local_irq_enable()
+///---???
+#include <linux/irqflags.h>
 
-#define verify_area(x,y,z) (access_ok(x,y,z) ? 0 : 1)
+#define dev_get(x) __dev_get_by_name(&init_net,x)
+
+extern spinlock_t chaos_lock;
+
+extern int chdebug;
+
+#ifdef DEBUG_CHAOS
+#define ASSERT(x,y)	if(!(x)) printk("%s: Assertion failure\n",y);
+#define trace		if (chdebug) printk
+#else
+#define ASSERT(x,y)
+#define trace		if (0) printk
+#endif
 
 #define printf printk
-
-#define ASSERT(x,msg)	WARN_ON(x)
-
-#define trace(fmt, args...) printk("%s:%d: %s: " fmt, __FILE__, __LINE__, __PRETTY_FUNCTION__, ##args)
