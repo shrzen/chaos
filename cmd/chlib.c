fc48254a62c5d15ecd36d8f23dcf839d154fc90b
Alfred M. Szmidt <ams@gnu.org>
2018-03-09 23:53:56 +0100

    cmd/chlib.c: List exported interfaces.

--- cmd/chlib.c
+++ cmd/chlib.c
@@ -1,3 +1,12 @@
+/*
+ * int chopen(int address, char *contact, int mode, int async, char *data, int dlength, int rwsize)
+ * int chlisten(char *contact, int mode, int async, int rwsize)
+ * int chreject(int fd, char *string)
+ * int chstatus(int fd, struct chstatus *chst)
+ * int chsetmode(int fd, int mode)
+ * int chwaitfornotstate(int fd, int state)
+ */
+
 #define NO_CHAOS_SOCKET 1
 
 #if NO_CHAOS_SOCKET
