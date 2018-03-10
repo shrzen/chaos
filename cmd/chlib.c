0d677f5a1daa895f9bb30e41dedad8a696d775f2
Alfred M. Szmidt <ams@gnu.org>
2018-03-10 20:00:19 +0100

    chlib.c: Add a bit of documentation.

--- cmd/chlib.c
+++ cmd/chlib.c
@@ -1,10 +1,32 @@
 /*
- * int chopen(int address, char *contact, int mode, int async, char *data, int dlength, int rwsize)
- * int chlisten(char *contact, int mode, int async, int rwsize)
- * int chreject(int fd, char *string)
- * int chstatus(int fd, struct chstatus *chst)
- * int chsetmode(int fd, int mode)
- * int chwaitfornotstate(int fd, int state)
+ * Calls that are independant of implementation:
+ *
+ *   int chopen(int address, char *contact, int mode, int async, char *data, int dlength, int rwsize)
+ *     Matching ioctl: CHIOCOPEN	
+ *   int chlisten(char *contact, int mode, int async, int rwsize)
+ *   int chreject(int fd, char *string)
+ *     Matching ioctl: CHIOCREJECT	
+ *   int chstatus(int fd, struct chstatus *chst)
+ *     Matching ioctl: CHIOCGSTAT	
+ *   int chsetmode(int fd, int mode)
+ *     Matching ioctl: CHIOCSMODE -- Set the mode of this channel
+ *   int chwaitfornotstate(int fd, int state)
+ *     Matching ioctl: CHIOCSWAIT -- Wait for a different state
+ *
+ * Calls that have no corresponding wrapper function:
+ *
+ *   CHIOCRSKIP		Skip the last read unmatched RFC 
+ *   CHIOCPREAD		Read my next data or control pkt 
+ *   CHIOCFLUSH		flush current output packet 
+ *   CHIOCANSWER	Answer an RFC (in RFCRECVD state) 
+ *   CHIOCACCEPT	Accept an RFC, opening connection 
+ *   CHIOCOWAIT		Wait until all output acked
+ *   CHIOCADDR		Set my address 
+ *   CHIOCNAME		Set my name 
+ *   CHIOCILADDR	Set chaos address for chil 
+ *   CHIOCETHER		Set chaos address for ether 
+ *   CHIOCGTTY		Hook up tty and return unit
+ *
  */
 
 #define NO_CHAOS_SOCKET 1
